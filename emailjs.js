79fec5972c31942b8337f4da794ffa3fd3a158f8
diego <reyesdiego@hotmail.com>
2017-04-25 21:30:13 -0300

    Email Emailjs

--- emailjs.js
+++ emailjs.js
@@ -1,22 +1,25 @@
 /**
  * Created by diego on 15/03/17.
  */
+
+var gmail = {
+    user:	"pietrandrea2000@gmail.com",
+    password: "belg4444",
+    host:	"smtp.gmail.com",
+    ssl: true
+};
+
 var email 	= require("emailjs");
-var server 	= email.server.connect({
-    user:	"reyesdiego@hotmail.com",
-    password:"",
-    host:	"smtp-mail.outlook.com",
-    tls: {ciphers: "SSLv3"}
-});
+var server 	= email.server.connect(gmail);
 
 var message	= {
     text:	"i hope this works",
-    from:	"you <pietran@hotmail.com>",
+    from:	"pietrandrea2000@gmail.com",
     to:		"reyesdiego@hotmail.com",
     subject:	"testing emailjs",
     attachment:
         [
-            {data:"<html>i <i>hope</i> this works!</html>", alternative:true}
+            {data: "<html>i <i>hope</i> this works!</html>", alternative:true}
         ]
 };
 server.send(message, function(err, message) { console.log(err || message); });
--- index.js
+++ index.js
@@ -6,25 +6,53 @@ var http = require('http');
 var path = require('path');
 
 //var methodOverride = require('method-override');
-//var bodyParser = require('body-parser');
-var multer  = require('multer')();
+var bodyParser = require('body-parser');
 
 
 var express = require('express');
 var app = express();
+app.use(bodyParser.json());
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(methodOverride());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,accept,access_token,X-Requested-With');
+    next();
+});
 app.use(express.static(__dirname));
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/sendMail', multer.array(), function (req, res) {
-    console.log(req.body);
-    res.end();
+app.post('/sendMail', function (req, res, next) {
+    var form = req.body;
+
+    console.log("FORM %j", form);
+    var gmail = {
+        user:	"pietrandrea2000@gmail.com",
+        password: "belg4444",
+        host:	"smtp.gmail.com",
+        ssl: true
+    };
+
+    var email 	= require("emailjs");
+    var server 	= email.server.connect(gmail);
+
+    var message	= {
+        text: form.text,
+        from: "Estudio Pietrandrea <pietrandrea2000@gmail.com>",
+        to: form.email,
+        subject: "Consulta Estudio Pietrandrea",
+        attachment:
+            [
+                {data: "<html><p>"+form.name+"</p><p>"+form.message+"</p><p>"+form.telefono+"</p></html>", alternative:true}
+            ]
+    };
+    server.send(message, function(err, message) { console.log(err || message); });
+
+    res.sendFile(path.join(__dirname + '/contact.html'));
 });
 
 app.listen(8080);
